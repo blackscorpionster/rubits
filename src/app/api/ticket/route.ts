ed4bc2d573f8b90a2fe68de034ae683a075f0495
Guillermo Hernandez <guillermod@benon.com>
2025-04-02 18:45:11 +1000

    Api calling db exmaple

--- src/app/api/ticket/route.ts
+++ src/app/api/ticket/route.ts
@@ -1,26 +1,19 @@
 import { NextResponse } from "next/server";
+import prisma from "../../../../lib/prisma";
 
 export async function GET(request: Request) {
-	// 	  const { searchParams } = new URL(request.url);
-	//   const ticketId = searchParams.get('ticketId');
-	//   const response = await fetch(`https://api.tickets.com/tickets/${ticketId}`, {
-	// 	method: 'GET',
-	// 	headers: {
-	// 	  'Content-Type': 'application/json',
-	// 	  'Authorization': `Bearer ${process.env.TICKET_API_KEY}`,
-	// 	},
-	//   });
-
-	//   if (!response.ok) {
-	// 	return new Response('Error fetching ticket', { status: response.status });
-	//   }
-
-	//   const data = await response.json();
-	return NextResponse.json({
-		ticketId: "12345",
-		ticketName: "Sample Ticket",
-		ticketStatus: "Open",
-		ticketDescription: "This is a sample ticket description.",
-		ticketCreatedAt: "2023-10-01T12:00:00Z",
+	const ticket = await prisma.ticket.findUnique({
+		where: {
+			id: "633e9cef-6bac-4846-b518-78db2530a207", // Replace with actual ticket ID
+		},
+		include: {
+			// Include any related data you need
+			// For example, if you have a User model related to Ticket
+			draw: true,
+		},
 	});
+
+	console.log("Ticket data:", ticket);
+
+	return NextResponse.json(ticket);
 }
