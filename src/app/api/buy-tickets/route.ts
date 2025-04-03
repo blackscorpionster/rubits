97b54ae7ac5eab37aea6040304e6a277d075d69b
Jakob Harvey <jakobh@benon.com>
2025-04-03 13:46:23 +1000

    Add import

--- src/app/api/buy-tickets/route.ts
+++ src/app/api/buy-tickets/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import { Ticket } from "../../play-page/types";
+import prisma from "../../../../lib/prisma";
 
 export async function POST(request: NextRequest) {
   try {
