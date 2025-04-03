8dbf0292d3a85a49142c640aabc260318c5d48cf
Jakob Harvey <jakobh@benon.com>
2025-04-03 13:08:53 +1000

    Include draw in DB req

--- src/app/api/buy-tickets/route.ts
+++ src/app/api/buy-tickets/route.ts
@@ -19,6 +19,7 @@ export async function POST(request: NextRequest) {
         purchasedBy: null,
         status: "intact",
       },
+      include: { draw: true },
       take: numTickets,
     });
 
