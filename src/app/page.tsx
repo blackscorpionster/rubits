5a368d2635e8938c48acee8712cbc1bda3488fe4
Guillermo Hernandez <guillermod@benon.com>
2025-04-03 15:15:25 +1000

    initial work, remove bg image and use it as overlay

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,11 @@
 
 import { useState } from "react";
 import { useRouter } from "next/navigation";
+import { GoogleGenAI } from "@google/genai";
 
 const Home: React.FC = () => {
 	const [email, setEmail] = useState<string>("");
+	const [aiPrompt, setAiPrompt] = useState<string>("");
 	const [error, setError] = useState<string>("");
 	const [loading, setLoading] = useState<boolean>(false);
 	const router = useRouter();
@@ -12,6 +14,36 @@ const Home: React.FC = () => {
 	const validateEmail = (email: string): boolean => {
 		const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 		return regex.test(email);
+	};
+
+	const generateImage = async (prompt: string) => {
+		const ai = new GoogleGenAI({
+			apiKey: "AIzaSyAqjKhunOoC9WpQMU8i5xiuR-6Jme_WS0M",
+		});
+
+		const contents =
+			"Can you create a low-resolution image of 200 pixels x 200 pixels for " +
+			prompt;
+
+		// Set responseModalities to include "Image" so the model can generate  an image
+		const response = await ai.models.generateContent({
+			model: "gemini-2.0-flash-exp-image-generation",
+			contents: contents,
+			config: {
+				responseModalities: ["Text", "Image"],
+			},
+		});
+
+		for (const part of response.candidates[0].content.parts) {
+			// Based on the part type, either show the text or save the image
+			if (part.text) {
+				console.log("Part 1", part.text);
+			} else if (part.inlineData) {
+				return part.inlineData.data;
+			}
+		}
+
+		return "";
 	};
 
 	const handleSubmit = async (e: React.FormEvent): Promise<void> => {
@@ -29,13 +61,17 @@ const Home: React.FC = () => {
 		}
 
 		setLoading(true);
+		console.log("Email:", email, "AI Prompt:", aiPrompt);
 
-		const response = await fetch(`/api/login?email=${encodeURIComponent(email)}`, {
-			method: "GET",
-			headers: {
-			  "Content-Type": "application/json",
-			},
-		});
+		const response = await fetch(
+			`/api/login?email=${encodeURIComponent(email)}`,
+			{
+				method: "GET",
+				headers: {
+					"Content-Type": "application/json",
+				},
+			}
+		);
 
 		let player = await response.json();
 
@@ -44,14 +80,25 @@ const Home: React.FC = () => {
 			const response = await fetch("/api/login", {
 				method: "POST",
 				headers: {
-				  "Content-Type": "application/json",
+					"Content-Type": "application/json",
 				},
 				body: JSON.stringify({ email: email }),
-			  });
+			});
 			player = await response.json();
 		}
 
 		localStorage.setItem("playerId", player.id);
+		try {
+			const base64Image = await generateImage(aiPrompt);
+			if (base64Image) {
+				localStorage.setItem("customImage", base64Image);
+			}
+			// We do not care if the image genaration fails, we just want to show the default image
+		} catch (error) {
+			console.error("Error generating image:", error);
+			return;
+		}
+
 		router.push("/play-page");
 		setLoading(false);
 	};
@@ -66,7 +113,7 @@ const Home: React.FC = () => {
 						</div>
 					)}
 
-					<div className="mb-4">
+					<div className="mb-4 flex flex-col gap-4">
 						<label htmlFor="email" className="block text-sm font-medium mb-2">
 							Email
 						</label>
@@ -79,6 +126,19 @@ const Home: React.FC = () => {
 							required
 							className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
 						/>
+						<label htmlFor="email" className="block text-sm font-medium mb-2">
+							Customise your ticket by telling us about something you like
+						</label>
+						<textarea
+							id="aiprompt"
+							name="aiprompt"
+							placeholder="e.g. I like dogs, I like cats, I like pizza"
+							className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+							rows={4}
+							cols={50}
+							maxLength={50}
+							onChange={(e) => setAiPrompt(e.target.value)}
+						></textarea>
 					</div>
 
 					<button
